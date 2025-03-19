23fa1860ab02f944eb17b5d6c51ac22a751d884d
manuelalacerda <manuelalacerda88@gmail.com>
2025-03-19 20:56:48 -0300

    Primeiro Projeto

---
+++ PrimeiroProjeto/src/br/com/fiap/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.fiap;
+
+public class PrimeiraClasse {
+    /*comentario de
+     multiplas linhas
+     */
+    public static void main(String[] args) {
+        //comentario de uma unica linha
+        System.out.println("Hello World!");
+    }
+}
