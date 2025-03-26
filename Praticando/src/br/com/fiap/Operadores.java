00ee3f6c1f85bab3ac2d7649cbd30c3c37d8b224
manuelalacerda <manuelalacerda88@gmail.com>
2025-03-25 22:55:09 -0300

    Exercicio praticando questionario e operadores

---
+++ Praticando/src/br/com/fiap/Operadores.java
@@ -0,0 +1,30 @@
+package br.com.fiap;
+
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println("*--*-----------------------------------*--*");
+        System.out.println("Operações aritimeticas numeros inteiro");
+        System.out.println();
+
+        int numero1 = 200, numero2 = 100;
+
+        int totalInt = numero1 + numero2;
+        int divisaoInt = numero1 / numero2;
+        int menosInt = numero1 - numero2;
+        int vezesInt = numero1 * numero2;
+
+        System.out.println("Divisão dos números " + numero1 + " / " + numero2 + " è: " + divisaoInt);
+        System.out.println();
+        System.out.println("Soma dos números " + numero1 + " + " + numero2 + " é: " + totalInt);
+        System.out.println();
+        System.out.println("Multiplicação dos números " + numero1 + " * " + numero2 + " è: " + vezesInt);
+        System.out.println();
+        System.out.println("Subtração dos nùmeros " + numero1 + " - " + numero2 + " è: " + menosInt);
+
+
+
+
+
+
+    }
+}
